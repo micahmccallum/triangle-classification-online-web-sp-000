3defa4ab6d565025de3b327ae5a18f2157e68919
Micah McCallum <micahmccallum@outlook.com>
2020-04-10 14:50:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,9 +3,8 @@ class Triangle
   attr_accessor :side_a, :side_b, :side_c
 
   def initialize(side_a, side_b, side_c)
-    @side_a = side_a
-    @side_b = side_b
-    @side_c = side_c
+    @sides = [side_a, side_b, side_c]
+    
   end
 
   def kind
@@ -41,6 +40,7 @@ class Triangle
   end
 
   def is_valid?
+    
     (@side_a > 0 && @side_b > 0 && @side_c > 0) &&
     ((@side_a + @side_b) > @side_c)
   end
