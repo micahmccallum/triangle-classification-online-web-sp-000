b7eee8dcacc0b1567f79d21a05aaba6cdead30bd
Micah McCallum <micahmccallum@outlook.com>
2020-04-10 14:59:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,8 +2,8 @@ require 'pry'
 class Triangle
   attr_accessor :sides
 
-  def initialize(side_a, side_b, side_c)
-    @sides = [side_a, side_b, side_c]
+  def initialize(sides, sides, sides)
+    @sides = [sides, sides, sides]
     binding.pry
   end
 
@@ -30,19 +30,19 @@ class Triangle
   end
 
   def is_isosceles?
-    (@side_a == @side_b && @side_a != @side_c) ||
-    (@side_b == @side_c && @side_b != @side_a) ||
-    (@side_a == @side_c && @side_a != @side_b)
+    (@sides[0] == @sides[1] && @sides[1] != @sides[2]) ||
+    (@sides[1] == @sides[2] && @sides[2] != @sides[0]) ||
+    (@sides[2] == @sides[0] && @sides[0] != @sides[1])
   end
 
   def is_scalene?
-    @side_a != @side_b && @side_b != @side_c && @side_a != @side_c
+    @sides != @sides && @sides != @sides && @sides != @sides
   end
 
   def is_valid?
 
-    (@side_a > 0 && @side_b > 0 && @side_c > 0) &&
-    ((@side_a + @side_b) > @side_c)
+    (@sides > 0 && @sides > 0 && @sides > 0) &&
+    ((@sides + @sides) > @sides)
   end
 
   class TriangleError < StandardError
